d678952741f68ea985e7ec40bbbb34e718f34034
João Teles <127795554+JoaoTeles87@users.noreply.github.com>
2024-08-20 10:42:49 -0300

    Update Adding PL/SQL commands

--- consultas.sql
+++ consultas.sql
@@ -111,3 +111,67 @@ CREATE VIEW visualizar_detentos AS
 SELECT NOME, CPF  
 FROM DETENTO 
 WHERE DATA_ENT BETWEEN TO_DATE('2024-01-01', 'YYYY-MM-DD') AND TO_DATE('2024-01-31', 'YYYY-MM-DD');
+
+--CONSULTAS PL/SQL
+
+--RECORD
+DECLARE
+    TYPE r_detento_type 
+    IS 
+    RECORD (
+        nome Detento.nome%TYPE,
+        cpf Detento.cpf%TYPE
+    );
+    r_detento_info r_detento_type;
+BEGIN
+    SELECT nome, cpf INTO r_detento_info
+    FROM Detento
+    WHERE cpf = '95543210987';
+    
+    DBMS_OUTPUT.PUT_LINE('Nome: ' || r_detento_info.nome || ' CPF: ' || r_detento_info.cpf);
+END;
+
+--TABELA ASSOCIATIVA
+DECLARE
+    TYPE tabela_nome IS TABLE OF Detento.nome%TYPE INDEX BY PLS_INTEGER;
+    nomes tabela_nome;
+    i PLS_INTEGER := 1;
+BEGIN
+    FOR pessoa IN (SELECT nome FROM Detento) LOOP
+        nomes(i) := pessoa.nome;
+        i := i + 1;
+    END LOOP;
+    
+    FOR presidiario IN 1..nomes.COUNT LOOP
+        DBMS_OUTPUT.PUT_LINE('Nome: ' || nomes(presidiario));
+    END LOOP;
+END;
+
+--BLOCO ANÔNIMO
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Bloco anônimo criado.');
+END;
+
+--PROCEDURE
+CREATE OR REPLACE PROCEDURE imprimir_salas (
+    id_Sala_visita NUMBER
+)     
+IS
+    r_salas Sala_visita%ROWTYPE;
+BEGIN
+  -- Buscando os dados da sala especificada
+  SELECT *
+  INTO r_salas
+  FROM Sala_visita
+  WHERE id = id_Sala_visita;
+
+  -- Exibindo os valores dos campos
+  dbms_output.put_line('ID da Sala: ' || r_salas.id);
+
+EXCEPTION
+   WHEN NO_DATA_FOUND THEN
+      dbms_output.put_line('Nenhuma sala encontrada com esse ID. Lembre que só existem salas de 101 - 110');
+   WHEN OTHERS THEN
+      dbms_output.put_line('Erro: ' || SQLERRM);
+END;
+
